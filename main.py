2dcb00d1ffd82b92149ffaf6416fe2d688b81f75
Nalini <24.nalini.k@gmail.com>
2025-11-23 11:49:40 +0530

    Modified index.html for GET method

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 # main.py
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from agents.parent_agent import build_response
 
 app = FastAPI(title="Tourism Multi-Agent API")
+from fastapi.middleware.cors import CORSMiddleware
 
-class RequestBody(BaseModel):
-    input: str
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["*"],
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
 
-@app.post("/plan")
-def plan_trip(req: RequestBody):
-    result = build_response(req.input)
+
+
+
+@app.get("/plan")
+def plan_trip(input: str = Query(..., description="User input text")):
+    result = build_response(input)
 
     if not result["ok"]:
         raise HTTPException(status_code=404, detail=result["message"])
